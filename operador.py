2e34a4c176547306640ae41dc9225b64bd58b8f9
Igor Manoel <igorssmanoel@gmail.com>
2023-03-05 22:45:57 +0000

    Exercicio

--- operador.py
+++ operador.py
@@ -1,4 +1,4 @@
 variavel_nome = input("Digite seu nome: ")
-print("Meu nome é " + variavel_nome) 
+print("Meu nome é " + variavel_nome)
 variavel_idade = input("Digite sua idade: ")
 print(f"O(a) {variavel_nome} possui {variavel_idade} anos")
