a3a67bb53f75ba0315c6e3cb323dbf05c7af62c2
Igor Manoel <igorssmanoel@gmail.com>
2023-03-10 15:14:40 +0000

    Rename folder

--- exercicios/ex01.py
+++ exercicios/ex01.py
@@ -2,4 +2,5 @@ nome = input("Digite seu nome: ")
 endereco = input("Digite seu endereco: ")
 profissao = input("Digite sua profissao: ")
 
-print(f"{nome} - {endereco} - {profissao}") # Alterar aqui
+#print("Nome: " + nome + "Endereço: " + endereco + "Profissão: " + profissao)
+print(f"Nome: {nome} - Endereço: {endereco} - Profissão: {profissao}")
